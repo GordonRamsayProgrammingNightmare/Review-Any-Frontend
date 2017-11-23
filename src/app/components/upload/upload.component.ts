533fae749e112e626b1becca70b4a7f1995ff303
JulianKwon <julian8748@gmail.com>
2017-11-23 19:21:09 +0900

    commit today

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -61,7 +61,7 @@ export class UploadComponent implements OnInit {
     this.uploader.uploadPost(this.post)
     .then((msg) => {
       console.log(msg.json());
-      alert(msg.json().message);
+      alert("Uploaded file successfully");
       this.router.navigateByUrl('/');
     })
     .catch((err) => {
