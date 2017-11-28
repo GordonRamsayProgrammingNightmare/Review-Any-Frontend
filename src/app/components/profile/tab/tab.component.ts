5b41f90d01728ddcf12d983da2b7ead7fd1b4947
lou297 <lou297@naver.com>
2017-11-28 20:31:41 +0900

    mypost

--- src/app/components/profile/tab/tab.component.ts
+++ src/app/components/profile/tab/tab.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'app/services/auth.service';
+import { GetDataService } from 'app/services/get-data.service';
+import { Post2 } from 'app/models/post';
 
 @Component({
   selector: 'app-tab',
@@ -6,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab.component.css']
 })
 export class TabComponent implements OnInit {
+  private token: any;
+  public posts: Array<any>;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private getData: GetDataService
+  ) {
+    this.token = localStorage.getItem('token');
+   }
 
   ngOnInit() {
+    this.getData.getData(this.token, 'post/all')
+    .then((data) => {
+      var p = [];
+      data.json().posts.forEach(element => {
+        p.push(element);
+      });
+      this.posts = p;
+
+    }).catch((err) => {
+      console.log('error: \n' + err);
+    });
+  }
+
+  viewHandler(postId): void {
+    this.getData.sendData(this.token, 'post/view', postId)
+      .then((msg) => {
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  likeBtnHandler(postId): void {
+    this.getData.sendData(this.token, 'post/like', postId)
+      .then((msg) => {
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
 }
