3f61246aee3e7acb5679b55ecb20bd966a83307d
JulianKwon <julian8748@gmail.com>
2017-11-29 20:32:44 +0900

    like post view post complete

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,7 @@ import { CrudDataService } from 'app/services/crud-data.service';
 import { Post2 } from 'app/models/post';
 import { forEach } from '@angular/router/src/utils/collection';
 import { setInterval } from 'timers';
+import { SimpleChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -27,6 +28,12 @@ export class HomeComponent implements OnInit {
     this.getUserLikePost();
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    for (let c in changes) {
+      console.log(c);
+    }
+  }
+
   ngOnInit() {
     this.updateData();
     // this.interval = setInterval(() => {
@@ -40,6 +47,7 @@ export class HomeComponent implements OnInit {
     .then((data) => {
       // console.log(data.json().posts);
       var p = [];
+      console.log(this.likePosts);
       data.json().posts.forEach(element => {
         if(this.chkLiked(element._id)) {
           element.isLiked = true;
@@ -77,8 +85,6 @@ export class HomeComponent implements OnInit {
           if(element._id == postId) {
             element.viewCnt = singlePost.viewCnt;
             element.likeCnt = singlePost.likeCnt;
-            // console.log(element.viewCnt);
-            // console.log(data.viewCnt);
           }
         });
       })
@@ -87,6 +93,7 @@ export class HomeComponent implements OnInit {
       });
   }
 
+  // 좋아요 목록 refresh
   getUserLikePost(): any {
     this.crudData.getData(this.token, 'user')
       .then(data => {
@@ -98,34 +105,43 @@ export class HomeComponent implements OnInit {
       });
   }
 
+  // 한 포스트 좋아요 했는지 체크
   chkLiked(id): boolean {
     this.getUserLikePost();
-    var isLiked: boolean = false;
+    var isLike: boolean = false;
     this.likePosts.forEach(element => {
       if(id == element) {
-        isLiked = true;
+        isLike = true;
       }
     });
-    return isLiked;
+    return isLike;
   }
 
   likeBtnHandler(postId, post): void {
-    console.log(this.chkLiked(postId));
-    console.log(post.isLiked);
+    // console.log(this.chkLiked(postId));
+    // console.log(post.isLiked);
     if(!this.chkLiked(postId) && !post.isLiked) {
       post.isLiked = true;
+      post.likeCnt += 1;
+      // console.log(post.likeCnt);
+      // 포스트 좋아요 db에 반영
       this.crudData.sendData(this.token, 'post/like', postId)
       .then((msg) => {
+        this.getUserLikePost();
         console.log(msg.json());
       })
       .catch(err => {
         console.log(err);
       });
-    };
+    }
     if (this.chkLiked(postId) && post.isLiked) {
       post.isLiked = false;
+      post.likeCnt -= 1;
+      console.log(post.likeCnt);
+      // 포스트 좋아요 취소 db에 반영
       this.crudData.deleteData(this.token, 'post/like', postId)
       .then((msg) => {
+        this.getUserLikePost();
         console.log(msg.json());
       })
       .catch(err => {
